c0bd644b7001f8f3038c77b4863bdfadb05228bc
Akif Islam <iamakifislam@gmail.com>
2022-06-18 13:18:20 +0600

    sample GIF added for ColourMyView

--- app/src/main/java/com/akifislam/aboutme/MainActivity.kt
+++ app/src/main/java/com/akifislam/aboutme/MainActivity.kt
@@ -1,40 +1,47 @@
-package com.akifislam.aboutme
+package com.akifislam.colourmyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        var nicknametext = findViewById<TextView>(R.id.nickname_text)
-        var nicknameedit = findViewById<EditText>(R.id.nicknameEdit)
-        var donebutton = findViewById<TextView>(R.id.done_btn)
+        setListeners()
+    }
 
 
+    private fun setListeners(){
+        val box_text_one: View = findViewById(R.id.box_one)
+        val box_text_two: View = findViewById(R.id.box_two)
+        val box_text_three: View = findViewById(R.id.box_three)
+        val box_text_four: View = findViewById(R.id.box_four)
+        val box_text_five: View = findViewById(R.id.box_five)
+        val constraint_layout: View = findViewById(R.id.constraint_layout)
 
-        var nickname = nicknameedit.text.toString()
-        nicknametext.text = nickname
+        val clickableViews: List<View> =
+            listOf(box_text_one, box_text_two, box_text_three, box_text_four, box_text_five, constraint_layout)
 
-
-        donebutton.setOnClickListener {
-            nicknametext.text = nicknameedit.text
-            nicknameedit.text.clear()
-            //view gone
-            nicknameedit.visibility = EditText.GONE
-            donebutton.visibility = Button.GONE
-            //Hide Keyboard
-            nicknameedit.clearFocus()
-            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-            inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
-
+        for(item in clickableViews) {
+            item.setOnClickListener{makeColored(it)}
         }
+    }
 
+    private fun makeColored(view: View) {
+        when (view.id) {
+            // Boxes using Color class colors for background
+            R.id.box_one ->
+                view.setBackgroundColor(Color.CYAN)
+            R.id.box_two -> view.setBackgroundColor(Color.MAGENTA)
+            // Boxes using Android color resources for background
+            R.id.box_three -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four-> view.setBackgroundResource(android.R.color.holo_orange_light)
+            R.id.box_five-> view.setBackgroundResource(android.R.color.black)
 
+            else -> view.setBackgroundColor(Color.rgb(Random.nextInt(256), Random.nextInt(256), Random.nextInt(256)))
+        }
     }
 }
